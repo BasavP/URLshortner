f513caf06e8fb31236bd716bc23d5f8155063cc2
basav <basav@outlook.in>
2026-05-17 18:13:56 +0530

    changes

--- src/core/config.py
+++ src/core/config.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "urlshortner"
-    app_env: str = "development"
-    database_url: str = "postgresql+psycopg://postgres:postgres@db:5432/urlshortner"
-    redis_url: str = "redis://redis:6379/0"
+    app_name: str
+    app_env: str
+    database_url: str
+    redis_url: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
